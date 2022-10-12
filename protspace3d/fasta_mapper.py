6dbde623c8e7a3a456b4295d4ad60904792be2a4
tsenoner <senoner@rostlab.org>
2022-10-12 12:31:53 +0200

    Fix bugs in fasta_mapper
    - option to provide CSV feature file
    - return new mapped feature file
    - return proper CSV file

--- protspace3d/fasta_mapper.py
+++ protspace3d/fasta_mapper.py
@@ -10,10 +10,11 @@ Usage:       python main.py
 """
 import argparse
 from itertools import product
-from math import ceil
-from pathlib import Path, PosixPath
+from math import log, ceil
+from pathlib import Path
 from typing import Generator
 
+import pandas as pd
 from pyfaidx import Fasta
 
 # import typer
@@ -23,12 +24,14 @@ class FastaMapper:
     """Maps the fasta file headers to alphabet like headers (AA AB ... ZY ZZ)"""
 
     seqs: Fasta
+    df_features: pd.DataFrame = None
 
     def __init__(
         self,
         fasta_input: Path,
         fasta_output: Path = None,
         csv_mapping: Path = None,
+        csv_features: Path = None,
     ) -> None:
         fasta_suffixes = [".fasta", ".fna", ".ffn", ".faa", ".frn", ".fa"]
         assert fasta_input.suffix in fasta_suffixes, (
@@ -46,18 +49,27 @@ class FastaMapper:
             if csv_mapping is not None
             else self.fasta_output.with_suffix(".csv")
         )
+        if csv_features is not None:
+            self.df_features = pd.read_csv(csv_features, sep=";")
+            self.df_features.iloc[:, 0] = self.df_features.iloc[:, 0].astype(
+                "str"
+            )
         self.cache = self.fasta_input.with_suffix(".fasta.fai")
 
     def _fasta_parser(self) -> None:
         # efficient pythonic random access to fasta subsequences
-        self.seqs = Fasta(self.fasta_input)
+        self.seqs = Fasta(
+            self.fasta_input,
+            read_long_names=True,
+            sequence_always_upper=True,
+        )
 
     def _get_new_seqs_ids(self) -> Generator[str, None, None]:
         # get number of sequences and calculate how many letters are needed to
         # uniquely identify all sequences
         nr_seqs = len(self.seqs.keys())
         alphabet = "".join(map(chr, range(ord("A"), ord("Z") + 1)))
-        nr_letters_needed = ceil(nr_seqs ** (1 / len(alphabet)))
+        nr_letters_needed = ceil(log(nr_seqs, len(alphabet)))
 
         # new identifier generator (e.g. AA, AB, AC, ...)
         new_seq_ids = (
@@ -75,16 +87,26 @@ class FastaMapper:
         self._fasta_parser()
         new_seq_ids = self._get_new_seqs_ids()
 
-        # create new FASTA and CSV file
-        with (
-            open(self.fasta_output, "w", encoding="utf-8") as fasta_handler,
-            open(self.csv_mapping, "w", encoding="utf-8") as map_handler,
-        ):
-            map_handler.write("mapped_id,original_id\n")
+        # create new FASTA and CSV
+        data = {"mapped_id": [], "original_id": []}
+        with (open(self.fasta_output, "w", encoding="utf-8") as fasta_handler,):
             for seq, new_seq_id in zip(self.seqs, new_seq_ids):
                 fasta_handler.write(f">{new_seq_id}\n")
                 fasta_handler.write(f"{seq}\n")
-                map_handler.write(f"{new_seq_id},{seq.long_name}\n")
+                data["mapped_id"].append(new_seq_id)
+                data["original_id"].append(seq.long_name)
+        df = pd.DataFrame(data)
+        if self.df_features is not None:
+            first_column_name = self.df_features.columns[0]
+            df = df.merge(
+                right=self.df_features,
+                left_on="original_id",
+                right_on=first_column_name,
+                how="outer",
+                copy=False,
+            )
+            df = df.drop(columns=[first_column_name])
+        df.to_csv(self.csv_mapping, index=False)
         print(f"Mapped FASTA file saved at: {self.fasta_output}")
         print(f"CSV mapping to old file: {self.csv_mapping}")
 
@@ -95,14 +117,13 @@ class FastaMapper:
 def parse_args():
     """Defines required and optional arguments to run the script
 
-    Run example: python fasta_mapping/main.py -fi example/example.fasta
+    Run example: python protspace3d/fasta_mapper.py -fi data/ex3/Conotoxins_try1.fasta -ff data/ex3/Conotoxins_try1.csv
     """
 
     # Instantiate the parser
     parser = argparse.ArgumentParser(
         description="Map fasta file to unique identifiers e.g. AA AB AC BA ..."
     )
-    # Required positional argument
     parser.add_argument(
         "-fi",
         "--fasta_input",
@@ -110,7 +131,6 @@ def parse_args():
         type=str,
         help="The .fasta file to be mapped",
     )
-    # Required positional argument
     parser.add_argument(
         "-fo",
         "--fasta_output",
@@ -121,7 +141,6 @@ def parse_args():
             " with `_mapped` added to the name)"
         ),
     )
-    # Required positional argument
     parser.add_argument(
         "-m",
         "--mapping",
@@ -132,6 +151,16 @@ def parse_args():
             " .csv as suffix). Column names: mapped_id, original_id"
         ),
     )
+    parser.add_argument(
+        "-ff",
+        "--feature_file",
+        required=False,
+        type=str,
+        help=(
+            "CSV file containing additional information to group the"
+            " embeddings. If given, the features will be added to the CSV file."
+        ),
+    )
 
     args = parser.parse_args()
     fasta_input = Path(args.fasta_input)
@@ -139,18 +168,21 @@ def parse_args():
         Path(args.fasta_output) if args.fasta_output is not None else None
     )
     csv_mapping = Path(args.mapping) if args.mapping is not None else None
+    csv_features = (
+        Path(args.feature_file) if args.feature_file is not None else None
+    )
 
-    return fasta_input, fasta_output, csv_mapping
+    return fasta_input, fasta_output, csv_mapping, csv_features
 
 
 def main():
     """Run main script"""
-    fasta_input, fasta_output, csv_mapping = parse_args()
-
+    fasta_input, fasta_output, csv_mapping, csv_features = parse_args()
     fasta_mapper = FastaMapper(
         fasta_input=fasta_input,
         fasta_output=fasta_output,
         csv_mapping=csv_mapping,
+        csv_features=csv_features,
     )
     fasta_mapper.create_mapping()
 
