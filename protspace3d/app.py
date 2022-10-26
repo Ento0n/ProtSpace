09bc5980d25426068bb9059e61a42391a3c7cecf
Anton <ge54kog@mytum.de>
2022-10-26 07:59:13 +0200

    Add differentiation between mapped and unmapped Check whether basename ends with "_mapped" and act accordingly.

--- protspace3d/app.py
+++ protspace3d/app.py
@@ -132,7 +132,7 @@ def main():
         structure_container = data_preprocessor.init_structure_container()
 
     # Create visualization object
-    visualizator = Visualizator(fig, csv_header, data_f)
+    visualizator = Visualizator(fig, csv_header)
 
     # --- APP creation ---
     application = visualizator.init_app()
