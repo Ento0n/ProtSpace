35bc438e3fb2eebe1808526f8fe4d3ad0f629249
tsenoner <senoner@rostlab.org>
2022-11-20 22:45:01 +0100

    Add PDB files for Pla2g2
    - add pdb and json files for Pla2g2
    - untrack colabfold directory containing all original colabfold outputs

--- script/get_best_pdbs.py
+++ script/get_best_pdbs.py
@@ -51,31 +51,38 @@ def get_proten_header(pdb_fn: PosixPath) -> str:
     return header
 
 
-def extract_rank1_models(from_dir: PosixPath, to_dir: PosixPath) -> None:
+def extract_rank1_models(from_dir: PosixPath, pdb_dir: PosixPath, json_dir: PosixPath) -> None:
     """Copy rank_1 files from `from_dir` to `to_dir` with header from `.a3m`
 
     *rank_1* files contain the best of the 5 predicted models. The `.pbd` file
     contains the 3D structure of the model itself whereas the `.json` file
     contains the scores (PAE, pLDDT, pTM).
     """
-    if not to_dir.is_dir():
-        to_dir.mkdir()
+    for directory in [pdb_dir, json_dir]:
+        if not directory.is_dir():
+            directory.mkdir()
 
     rank1_files = from_dir.glob("*rank_1*")
     for pdb_file in rank1_files:
-        # print(pdb_file)
         header = get_proten_header(pdb_file)
-        new_pdb_fn = to_dir / f"{header}{pdb_file.suffix}"
+        suffix = pdb_file.suffix
+        if suffix == ".pdb":
+            new_fn = pdb_dir / f"{header}{pdb_file.suffix}"
+        elif suffix == ".json":
+            new_fn = json_dir / f"{header}{pdb_file.suffix}"
+        else:
+            raise ValueError(f"Suffix type `{suffix}` is not valide.")
         # print(new_pdb_fn)
-        if not new_pdb_fn.is_file():
-            shutil.copy(pdb_file, new_pdb_fn)
+        if not new_fn.is_file():
+            shutil.copy(pdb_file, new_fn)
 
 
 def main():
     colabfold_dir = setup_arguments()
     pdb_dir = colabfold_dir.parent / "pdb"
+    json_dir = colabfold_dir.parent / "json"
 
-    extract_rank1_models(from_dir=colabfold_dir, to_dir=pdb_dir)
+    extract_rank1_models(from_dir=colabfold_dir, pdb_dir=pdb_dir, json_dir=json_dir)
 
 
 if __name__ == "__main__":
