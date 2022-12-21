aef21b2196a7e284da14553ca25ffc81be3efcdf
Anton <ge54kog@mytum.de>
2022-12-21 15:34:32 +0100

    Delete unnessecary code in children of info toast

--- protspace3d/visualization.py
+++ protspace3d/visualization.py
@@ -471,45 +471,9 @@ class Visualizator:
     def get_info_toast():
         toast = dbc.Toast(
             children=[
-                html.P("Sequence:"),
-                html.Div(
-                    id="collapsed_seq_div",
-                    hidden=False,
-                    children=[
-                        dbc.Row(
-                            [
-                                dbc.Col(
-                                    [
-                                        html.P("Sequence is here!"),
-                                    ],
-                                    width=6,
-                                ),
-                                dbc.Col(
-                                    [
-                                        html.Button(
-                                            "...",
-                                            id="expand_seq_button",
-                                            style={
-                                                "padding": 0,
-                                                "border": "none",
-                                                "background": "none",
-                                            },
-                                        )
-                                    ],
-                                    width=6,
-                                ),
-                            ]
-                        ),
-                    ],
-                ),
-                html.Div(
-                    id="expanded_seq_div",
-                    hidden=True,
-                    children=[
-                        html.P("Complete sequence shown here!"),
-                    ],
-                ),
-                html.P(f"Seq. length: 0"),
+                html.Div(id="expanded_seq_div"),
+                html.Div(id="collapsed_seq_div"),
+                html.Button(id="expand_seq_button"),
             ],
             id="info_toast",
             is_open=False,
