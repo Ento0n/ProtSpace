48309f490e4615591dbae80d22f1ea1c0b510bc9
Anton <ge54kog@mytum.de>
2023-02-27 11:39:26 +0100

    Add script that turns project data into tensorflow embedding projector format (tsv)

---
+++ src/side_scripts/2_embedding_projector.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import argparse
+from pathlib import Path
+import h5py
+import pandas as pd
+
+# Arguemnt parser #
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-hdf", required=True, type=str, help="Path to the h5 file.")
+parser.add_argument("-o", required=True, type=str, help="Output directory for the tsv file(s)")
+parser.add_argument("-csv", required=False, type=str, help="Path to the csv file (Labels).")
+args = parser.parse_args()
+
+hdf_path = Path(args.hdf) if args.hdf is not None else None
+out_path = Path(args.o) if args.o is not None else None
+csv_path = Path(args.csv) if args.csv is not None else None
+
+# Read Files #
+
+embd_list = list()
+uid_list = list()
+with h5py.File(hdf_path, "r") as hdf:
+    for identifier, embeddings in hdf.items():
+        embd_list.append(embeddings[:])
+        uid_list.append(identifier)
+
+df_emb = pd.DataFrame(embd_list, index=uid_list)
+
+df_csv = pd.read_csv(csv_path, index_col=0)
+
+# Fit files to IDs #
+
+if len(df_emb) > len(df_csv):
+    df_emb = df_emb.loc[df_emb.index.isin(df_csv.index)]
+elif len(df_emb) < len(df_csv):
+    df_csv = df_csv.loc[df_csv.index.isin(df_emb.index)]
+
+df_emb, df_csv = df_emb.align(df_csv, join="left", axis=0) # align dataframes by Index
+
+# Save files #
+
+embd_tsv_path = out_path / hdf_path.stem
+embd_tsv_path = embd_tsv_path.with_suffix(".tsv")
+df_emb.to_csv(embd_tsv_path, sep="\t", header=False, index=False)
+
+label_tsv_path = out_path / csv_path.stem
+label_tsv_path = label_tsv_path.with_suffix(".tsv")
+df_csv.to_csv(label_tsv_path, sep="\t", index_label="index_name")
